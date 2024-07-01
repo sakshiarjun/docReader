7c6e12bfcef43ce1d3224b5567acbac0bcca42e9
sakshiarjun <sakshi.arjun04@gmail.com>
2024-07-01 10:55:43 +0530

    integrated faiss

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 #pip install langchain
 #pip install -U langchain-community
 #pip install unstructured unstructured[pdf]
+#pip install tiktoken
+#pip install faiss-cpu
 
 import streamlit as st
 import pandas as pd
@@ -11,52 +13,35 @@ import json
 from openai import OpenAI
 from dotenv import load_dotenv
 from langchain.document_loaders import UnstructuredFileLoader
+#from langchain_community.document_loaders import OnlinePDFLoader use for remote pdfs
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
 #from io import StringIO
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.org_id = os.getenv('ORG_ID')
-
 st.title("docReader")
-#uploaded_file = st.file_uploader("Choose a file (preferably .txt)")
-
-#df = pd.read_fwf(uploaded_file)
-
-#if uploaded_file is not None:
-    #stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    #st.write(stringio)
-    #string_data = stringio.read()
-    #st.write(string_data)
-        
-st.write("file uploaded from backend")
-loader = UnstructuredFileLoader("/home/sakshi/Desktop/docReader/Brown-Giving-PsychSci-2003.pdf")  #need filepath, cant get from st.file_uploader
-pages = loader.load_and_split()
-type(pages[0])
-text_splitter = CharacterTextSplitter(separator='\n', chunk_size=1000, chunk_overlap=200)
-text_chunks = text_splitter.split_documents(pages)
-st.write(text_chunks)
 
 client = OpenAI()
 def get_embedding(text, model='text-embedding-ada-002'):
     response = client.embeddings.create(input=[text], model=model)
     embeddings = response['data'][0]['embedding']
     return embeddings
-def embed_text_chunks(text_chunks, model='text-embedding-ada-002', output_file='/home/sakshi/Desktop/docReader/embeddings.json'):
-    embeddings_dict = {}
-    for i, chunk in enumerate(text_chunks):
-        embeddings = get_embeddings(chunk, model=model)
-        embeddings_dict[f'chunk_{i}'] = embeddings
-    with open(output_file, 'w') as f:
-        json.dump(embeddings_dict, f)
-def print_embeddings_file(file_path='/home/sakshi/Desktop/docReader/embeddings.json'):
-    with open(file_path, 'r') as f:
-        embeddings_dict = json.load(f)
-    st.write(json.dumps(embeddings_dict, indent=4))
-print_embeddings_file()
 
-#def response(question, get_embedding()):
-    #q_embedding = get_embedding(question)
+        
+st.write("file uploaded from backend")
+loader = UnstructuredFileLoader("/home/sakshi/Desktop/docReader/Taj_Mahal.pdf")  #need filepath, cant get from st.file_uploader
+pages = loader.load_and_split()
+type(pages[0])
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+text_chunks = text_splitter.split_documents(pages)
+#st.write(text_chunks)
+
+faiss_index = FAISS.from_documents(text_chunks, OpenAIEmbeddings(model='text-embedding-ada-002'))
 input_q = st.text_area("Enter question: ")
-#output_a = 
+answers = faiss_index.similarity_search(input_q,k=3)
+for i in answers:
+    st.write(i.page_content)
+    st.markdown("---")
